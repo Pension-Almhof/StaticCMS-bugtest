858218513778f87fd4505e964a0ef99cfe399d15
ddorstijn <dannydorstijn@1997@gmail.com>
2023-10-24 17:59:43 +0200

    Slimmed packages needed to install

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
 import prefetch from "@astrojs/prefetch";
-import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), prefetch(), icon({include: { ph: ['*'], emojione: ['*'] }}),],
+  integrations: [tailwind(), react(), prefetch()],
   vite: {
     ssr: {
       external: ["svgo"],
